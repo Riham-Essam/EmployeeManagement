695cfddb3c4652e81330bac09639cc3dee98ee8b
Riham Essam <rihamessam1234@gmail.com>
2023-12-19 01:33:25 +0200

    Delete User
    Add DeleteUser httpPost req
    Edit in ListUsers view

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -285,5 +285,33 @@ namespace EmployeeManagement.Controllers
 
 
         }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = $"User with ID = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListUsers");
+            }     
+        }
     }
 }
