c5a7faa6c0f77c992b9c8591e6f7637934528f9f
Riham Essam <rihamessam1234@gmail.com>
2023-12-19 16:19:20 +0200

    Update .gitignore

---
+++ EmployeeManagement/wwwroot/js/customScript.js
@@ -0,0 +1,17 @@
+﻿function confirmDelete(uniqueID, isDeletedClicked) {
+
+    var deleleUser = 'Delete_' + uniqueID;
+    var confirmDelete = 'ConfirmDelete_' + uniqueID;
+
+    if (isDeletedClicked) {
+
+        $('#' + deleleUser).hide();
+        $('#' + confirmDelete).show();
+
+    } else {
+
+        $('#' + deleleUser).show();
+        $('#' + confirmDelete).hide();
+
+    }
+}
